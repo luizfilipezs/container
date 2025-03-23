0de589c1cd8fac204959c9f010d0723d889de5e8
Luiz Filipe da Silva <luiz.silva@itmapps.com.br>
2025-03-23 15:09:53 -0300

    Add PHP CS Fixer "no_unused_imports" rule

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -9,6 +9,7 @@ return (new Config())
     ->setRules([
         'ordered_imports' => ['sort_algorithm' => 'alpha'],
         'group_import' => true,
+        'no_unused_imports' => true,
         'blank_line_after_namespace' => true,
         'single_line_after_imports' => true,
         'blank_line_before_statement' => [
