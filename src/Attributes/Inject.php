3e22e97c797edf0697c304ed8d191e63e7f6de95
Luiz Filipe da Silva <luiz.silva@itmapps.com.br>
2025-03-24 19:58:09 -0300

    Add docs to Inject attribute

--- src/Attributes/Inject.php
+++ src/Attributes/Inject.php
@@ -4,9 +4,17 @@ namespace Luizfilipezs\Container\Attributes;
 
 /**
  * Binds a parameter to a symbolic name representing a value to be injected.
+ *
+ * Note that the injection will be prioritized over the default argument.
  */
 #[\Attribute(\Attribute::TARGET_PARAMETER)]
 class Inject
 {
+    /**
+     * Construtor.
+     *
+     * @param string $identifier Injection identifier (key set via `Container::setValue` or
+     * any class name).
+     */
     public function __construct(public string $identifier) {}
 }
